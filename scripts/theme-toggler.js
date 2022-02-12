60f76cb4548597f1d7d6ead95c1d25c9e50b530c
Waldemar Glaz <ania@Waldemars-MacBook-Pro.local>
2022-02-12 19:23:34 +0100

    dark mode background colors

--- scripts/theme-toggler.js
+++ scripts/theme-toggler.js
@@ -2,12 +2,20 @@ const toggleBtn = document.querySelector('.theme-selector');
 const container = document.querySelector('.container');
 const logo = document.querySelector('.logo');
 const togglerIcon = document.querySelector('.theme-selector__icon');
-const togglerText = document.querySelector('.theme-selector__text')
+const togglerText = document.querySelector('.theme-selector__text');
+const searchBar = document.querySelector('.search-bar');
+const searchBarInput = document.querySelector('.search-bar__input');
+const result = document.querySelector('.result');
+const stats = document.querySelector('.user-stats');
 
 toggleBtn.addEventListener('click', ()=>{
     container.classList.toggle('dark');
     logo.classList.toggle('dark');
     toggleBtn.classList.toggle('dark');
+    searchBar.classList.toggle('dark');
+    searchBarInput.classList.toggle('dark');
+    result.classList.toggle('dark');
+    stats.classList.toggle('dark');
     if(toggleBtn.classList.contains('dark')){
         togglerIcon.src = './assets/icon-sun.svg';
         togglerText.textContent = 'LIGHT';
