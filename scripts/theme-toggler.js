f4376c488596ae8c8cb300e71160305ac4bb9fb2
Waldemar Glaz <ania@Waldemars-MacBook-Pro.local>
2022-02-14 10:38:31 +0100

    .body added, flex to .body

--- scripts/theme-toggler.js
+++ scripts/theme-toggler.js
@@ -1,5 +1,5 @@
 const toggleBtn = document.querySelector('.theme-selector');
-const container = document.querySelector('.container');
+const body = document.querySelector('.body');
 const logo = document.querySelector('.logo');
 const togglerIcon = document.querySelector('.theme-selector__icon');
 const togglerText = document.querySelector('.theme-selector__text');
@@ -9,7 +9,7 @@ const result = document.querySelector('.result');
 const stats = document.querySelector('.user-stats');
 const detailIcons = document.getElementsByClassName('detail__icon')
 toggleBtn.addEventListener('click', ()=>{
-    container.classList.toggle('dark');
+    body.classList.toggle('dark');
     logo.classList.toggle('dark');
     toggleBtn.classList.toggle('dark');
     searchBar.classList.toggle('dark');
