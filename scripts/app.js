a2c6349611a97209b3c08a513a0beb305e2757fb
Waldemar Głaz <w.glaz@cheil.com>
2022-04-06 01:44:42 +0200

    validateData added, twitter_username fixed

--- scripts/app.js
+++ scripts/app.js
@@ -1,95 +1,81 @@
-const gitHubUrl = 'https://api.github.com/users/';
-const inputEl = document.querySelector('.search-bar__input');
-const nameEl = document.querySelector('.result__user-name');
-const usernameEl = document.querySelector('.result__user-username');
-const avatar = document.querySelector('.result__avatar');
-const userBio = document.querySelector('.bio');
-const searchBtn = document.querySelector('.search-bar__btn');
-const repos = document.querySelector('.repos');
-const followers = document.querySelector('.followers');
-const following = document.querySelector('.following');
-const city = document.querySelector('.city');
-const website = document.querySelector('.website');
-const twitter = document.querySelector('.twitter');
-const company = document.querySelector('.company');
-const joinedDate = document.querySelector('.result__user-join-date');
-const errorMsg = document.querySelector('.error')
+const gitHubUrl = "https://api.github.com/users/";
+const inputEl = document.querySelector(".search-bar__input");
+const nameEl = document.querySelector(".result__user-name");
+const usernameEl = document.querySelector(".result__user-username");
+const avatar = document.querySelector(".result__avatar");
+const userBio = document.querySelector(".bio");
+const searchBtn = document.querySelector(".search-bar__btn");
+const repos = document.querySelector(".repos");
+const followers = document.querySelector(".followers");
+const following = document.querySelector(".following");
+const city = document.querySelector(".city");
+const website = document.querySelector(".website");
+const twitter = document.querySelector(".twitter");
+const company = document.querySelector(".company");
+const joinedDate = document.querySelector(".result__user-join-date");
+const errorMsg = document.querySelector(".error");
 
+searchBtn.addEventListener("click", (e) => {
+  errorMsg.classList.remove("active");
+  e.preventDefault();
+  let username = inputEl.value;
 
-
-searchBtn.addEventListener('click', (e)=>{
-  errorMsg.classList.remove('active')
-e.preventDefault();
-let username = inputEl.value;
-
-fetch(`${gitHubUrl}${username}`)
- .then(resp =>{
-   if(!resp.ok){
-     throw Error(resp.statusText)
-   }
-   return resp.json()
- })
- .then(data =>{
-   getData(data)
- })
- .catch(err => {
-   console.log('Not available');
-   errorMsg.classList.add('active')
-   console.log(err)
- })
+  fetch(`${gitHubUrl}${username}`)
+    .then((resp) => {
+      if (!resp.ok) {
+        throw Error(resp.statusText);
+      }
+      return resp.json();
+    })
+    .then((data) => {
+      getData(data);
+    })
+    .catch((err) => {
+      console.log("Not available");
+      errorMsg.classList.add("active");
+      console.log(err);
+    });
 });
 
 // Display default octocat account on start
-let username = 'octocat'
+let username = "octocat";
 
 fetch(`${gitHubUrl}${username}`)
-.then(response => response.json())
-.then(data => {
-  getData(data)
+  .then((response) => response.json())
+  .then((data) => {
+    getData(data);
   });
 
-function clearInput(){
-  inputEl.value = ''
+function clearInput() {
+  inputEl.value = "";
 }
 
-  function getData(data){
-    console.log(data);
-    nameEl.textContent = data['name'];
-    usernameEl.textContent = `@${data['login']}`;
-    avatar.src = data['avatar_url'];
-    if(data['bio'] === null){
-      userBio.textContent = 'This profile has no bio'
-    }else{
-      userBio.textContent = data['bio'];
-    }
-    
-    repos.textContent = data['public_repos'];
-    followers.textContent = data['followers'];
-    following.textContent = data['following'];
-    
-    if(data['twitter-username'] != null){
-      twitter.textContent = data['twitter-username']
-    }else{
-      twitter.textContent = 'Not Available';
-    }
-    if(data['company'] != null){
-      company.textContent = data['company']
-    }else{
-      company.textContent = 'Not Available';
-    }
-    if(data['blog'] != null){
-      website.textContent = data['blog']
-    }else{
-      website.textContent = 'Not Available';
-    }
-    if(data['location'] != null){
-      city.textContent = data['location']
-    }else{
-      city.textContent = 'Not Available';
-    }
-    let joinedDateRaw = data['created_at'].split('T')[[0]];
-    e = new Date(joinedDateRaw).toString()
-    new_arr = e.split(' ')
-    joinedDate.textContent = `Joined ${new_arr[2]} ${new_arr[1]} ${new_arr[3]}`;
-    clearInput()
-  }
+function getData(data) {
+  nameEl.textContent = data["name"];
+  usernameEl.textContent = `@${data["login"]}`;
+  avatar.src = data["avatar_url"];
+
+  userBio.textContent =
+    data["bio"] === null ? "This profile has no bio" : data["bio"];
+
+  repos.textContent = data["public_repos"];
+  followers.textContent = data["followers"];
+  following.textContent = data["following"];
+
+  const validateData = (data, element) =>
+    (element.textContent = data != null && data != "" ? data : "Not Available");
+
+  validateData(data["twitter_username"], twitter);
+  validateData(data["company"], company);
+  validateData(data["blog"], website);
+  validateData(data["location"], city);
+
+  const joinedDateRaw = data["created_at"]
+    .split("T")
+    [[0]].toString()
+    .split("-");
+
+  const [year, month, day] = joinedDateRaw;
+  joinedDate.textContent = `Joined ${day} ${month} ${year}`;
+  clearInput();
+}
